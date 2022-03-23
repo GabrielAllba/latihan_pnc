a9da5b70f609af7d9f87bb27071e77c33d9caff9
GabrielAllba <rielgasy9@gmail.com>
2022-03-23 16:44:33 +0700

    initiation

---
+++ latihan_1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef char string[50];
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	string h = "halo riel";
+	int menu, i;
+	printf("%s", h);
+
+	int length = strlen(h);
+	printf("%d\n", length);
+	
+	for(i=100; i >= 1; i--){
+	}
+	switch(menu){
+		case 1:
+			break;
+		case 2:
+			break;
+		default: 
+	}while(menu!=0);
+	
+	return 0;
+	
+}
